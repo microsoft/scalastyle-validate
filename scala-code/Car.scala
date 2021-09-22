968c86ae1f749a43d96d51bb91524e60d0053791
Vianey Juarez <37337540+vianeyja@users.noreply.github.com>
2021-09-22 14:40:49 -0500

    Fix: Updated Scala code

--- scala-code/Car.scala
+++ scala-code/Car.scala
@@ -1,16 +1,5 @@
-class Car
-{
-
-    // Class variables
-    var make: String = "BMW"
-    var model: String = "X7"
-    var fuel: Int = 40
-
-    // Class method
-    def Display()
-    {
-       println("Make of the Car : " + make);
-       println("Model of the Car : " + model);
-       println("Fuel capacity of the Car : " + fuel);
-    }
+class Car {
+  def display = {
+    val x = 2
+  }
 }
